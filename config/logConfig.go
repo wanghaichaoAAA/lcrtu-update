295847f320016ea98d0e49e01b6ddd0bc25938c7
wanghaichao <wanghaichao@jslcinfo.com>
2020-10-10 09:26:35 +0800

    add:日志本地保存

--- config/logConfig.go
+++ config/logConfig.go
@@ -1,11 +1,27 @@
 package config
 
 import (
+	"fmt"
+	"github.com/lestrrat-go/file-rotatelogs"
 	log "github.com/sirupsen/logrus"
+	"io"
 	"os"
+	"time"
 )
 
 func init() {
+	filePath := "./log/lcrtu-update.log.%Y%m%d%H%M"
+	fileWriter, err := rotatelogs.New(
+		filePath,
+		rotatelogs.WithLinkName(filePath), // 生成软链，指向最新日志文件
+		rotatelogs.WithMaxAge(time.Hour*24*time.Duration(30)),      // 文件最大保存时间
+		rotatelogs.WithRotationTime(time.Hour*24*time.Duration(7)), // 日志切割时间间隔
+	)
+	if err != nil {
+		fmt.Println("初始化日志失败")
+		os.Exit(1)
+	}
+	log.SetOutput(io.MultiWriter(os.Stdout, fileWriter))
 	log.SetFormatter(&log.TextFormatter{
 		TimestampFormat: "2006-01-02 15:04:05",
 		DisableColors:   true,
--- go.mod
+++ go.mod
@@ -3,8 +3,14 @@ module lcrtu-update
 go 1.13
 
 require (
+	github.com/fastly/go-utils v0.0.0-20180712184237-d95a45783239 // indirect
 	github.com/gin-gonic/gin v1.6.3
+	github.com/jehiah/go-strftime v0.0.0-20171201141054-1d33003b3869 // indirect
+	github.com/jonboulle/clockwork v0.2.2 // indirect
+	github.com/lestrrat-go/file-rotatelogs v2.4.0+incompatible
+	github.com/lestrrat-go/strftime v1.0.3 // indirect
 	github.com/pkg/errors v0.9.1
 	github.com/robfig/cron/v3 v3.0.1
 	github.com/sirupsen/logrus v1.7.0
+	github.com/tebeka/strftime v0.1.5 // indirect
 )
